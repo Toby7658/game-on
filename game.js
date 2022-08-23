3994f40d7ffa21b49f6dfc58ba1ab36b65db6f16
Toby7658 <orlaghtobin@gmail.com>
2022-08-23 19:13:50 +0000

    Add images to image folder

--- game.js
+++ game.js
@@ -1,22 +1,92 @@
-/* columns and rows are 5x5 */
 var rows = 5;
 var columns = 5;
 
-/* swap tiles on board */
 var currTile;
 var otherTile;
 
 var turns = 0;
 
-
 window.onload = function() {
-    for (let r = 0; r < rows; r++ ) 
-    for (let c = 0; c < columns; c++) {
+    //initialize the 5x5 cubes
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            //<img>
+            let tile = document.createElement("img");
+            tile.src = "./images/blank.jpg";
 
-/* set structure for images on tiles */
-        let tile = document.createElement("img");
-        
+            //DRAG FUNCTIONALITY
+            tile.addEventListener("dragstart", dragStart); //click on image to drag
+            tile.addEventListener("dragover", dragOver);   //drag an image
+            tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+            tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+            tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+            tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
 
-        document.getElementById("block").append(tile);
+            document.getElementById("cubes").append(tile);
+        }
     }
+
+    //segment
+    let segment = [];
+    for (let i=1; i <= rows*columns; i++) {
+        segment.push(i.toString()); //put "1" to "25" into the array (puzzle images names)
+    }
+    segment.reverse();
+    for (let i =0; i < segment.length; i++) {
+        let j = Math.floor(Math.random() * segment.length);
+
+        //swap
+        let tmp = segment[i];
+        segment[i] = segment[j];
+        segment[j] = tmp;
+    }
+
+    for (let i = 0; i < segment.length; i++) {
+        let tile = document.createElement("img");
+        tile.src = "assets/images/." + segment[i] + ".jpg";
+
+        //DRAG FUNCTIONALITY
+        tile.addEventListener("dragstart", dragStart); //click on image to drag
+        tile.addEventListener("dragover", dragOver);   //drag an image
+        tile.addEventListener("dragenter", dragEnter); //dragging an image into another one
+        tile.addEventListener("dragleave", dragLeave); //dragging an image away from another one
+        tile.addEventListener("drop", dragDrop);       //drop an image onto another one
+        tile.addEventListener("dragend", dragEnd);      //after you completed dragDrop
+
+        document.getElementById("segment").append(tile);
+    }
+}
+
+//DRAG TILES
+function dragStart() {
+    currTile = this; //this refers to image that was clicked on for dragging
+}
+
+function dragOver(e) {
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    e.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+    otherTile = this; //this refers to image that is being dropped on
+}
+
+function dragEnd() {
+    if (currTile.src.includes("blank")) {
+        return;
+    }
+    let currImg = currTile.src;
+    let otherImg = otherTile.src;
+    currTile.src = otherImg;
+    otherTile.src = currImg;
+
+    turns += 1;
+    document.getElementById("turns").innerText = turns;
 }
